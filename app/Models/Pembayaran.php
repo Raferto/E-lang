b55cf9de9e8fdcf3bea8c261a3fb25b1969c26ba
rafi nizar <rafi.nizar15@gmail.com>
2021-04-30 15:23:27 +0700

    create: model pembayaran

---
+++ app/Models/Pembayaran.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pembayaran extends Model
+{
+    use HasFactory;
+    protected $table = "pembayaran";
+
+    protected $fillable = [
+        'user_id',
+        'penawaran_id',
+        'status',
+        'bukti_pembayaran',
+        'deadline_pembayaran'
+    ];
+
+}
