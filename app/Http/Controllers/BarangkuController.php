bb0deca2f3b1cee004b39b6337e15fd0cd4b9c10
Raferto <57932819+Raferto@users.noreply.github.com>
2021-05-02 12:28:36 +0700

    change
    pengajuan > barangku

--- app/Http/Controllers/BarangkuController.php
+++ app/Http/Controllers/BarangkuController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Barang;
 
-class PengajuanController extends Controller
+class BarangkuController extends Controller
 {
     public function index(){
         $barang = barang::paginate(5);
