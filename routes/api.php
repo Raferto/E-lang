cad81b878602a8109ebbb715c006247904984cdc
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2021-06-22 23:03:17 +0700

    create api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('bid')->name('bid.')->group(function () {
+    Route::get('/', [API\BidController::class, 'index'])->name('index');
+    Route::post('/', [API\BidController::class, 'create'])->name('create');
 });
