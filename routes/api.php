e16075c53c48172c5c707f7be330ea01e6801039
rafi nizar <rafi.nizar15@gmail.com>
2021-06-25 15:38:51 +0700

    updated: routing api klaim

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,17 @@ Route::post('register', [API\AuthController::class, 'register']);
 Route::post('login', [API\AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group( function () {
+    // route bid
     Route::prefix('bid')->name('bid.')->group(function () {
         Route::get('/', [API\BidController::class, 'index'])->name('index');
         Route::post('/', [API\BidController::class, 'create'])->name('create');
     });
+
+    // route klaim/pembayaran
+    Route::prefix('klaim')->name('klaim.')->group(function () {
+        Route::post('/', [API\KlaimAPIController::class, 'index'])->name('index');
+        Route::post('/show', [API\KlaimAPIController::class, 'show'])->name('show');
+        Route::post('/create', [API\KlaimAPIController::class, 'create'])->name('create');
+    });
 });
+
